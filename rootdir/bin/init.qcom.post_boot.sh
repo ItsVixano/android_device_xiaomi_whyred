d1f167c0a32d4aef229a2c406d1c7e67573eaa9b
Max Weffers <rcstar6696@gmail.com>
2020-05-18 12:51:26 +0200

    whyred: Start vendor.cdsprpcd only for sdm660
    * disable it for sdm636 and sdm630
    * fixes logspam on twolip and whyred with cdsprpcd not able to start
    
    [ItsVixano: just nuke it, we aren't sharing this trees with other devices]
    Change-Id: I4de5333709c2fb57f315b47842fbd12d4d80c36d
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2744,9 +2744,6 @@ case "$target" in
             # enable LPM
             echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 
-            # Start cdsprpcd only for sdm660 and disable for sdm630
-            start vendor.cdsprpcd
-
             # Start Host based Touch processing
                 case "$hw_platform" in
                         "MTP" | "Surf" | "RCM" | "QRD" )
