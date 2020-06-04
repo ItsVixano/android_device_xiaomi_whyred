4d9c218f830505a11a435cabeac52bd54a73ff4c
jean <jeangraff30@gmail.com>
2020-06-04 12:58:38 -0400

    whyred: Adapt ANT+ for CAF.
    Signed-off-by: jean <jeangraff30@gmail.com>
    Change-Id: I590cd90e04758e19e824e15765430db66beaed94

--- device.mk
+++ device.mk
@@ -142,10 +142,7 @@ $(call inherit-product, vendor/qcom/opensource/commonsys-intf/bluetooth/bt-syste
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app
 
 # Biometrics
 PRODUCT_PACKAGES += \
