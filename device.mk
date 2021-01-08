AOSiP-Devices/device_xiaomi_whyred

whyred: Add powerhint.json for sdm636
Change-Id: I3591038a29326b15ce00de0051208219d9f3830d

device.mk
@@ -61,6 +61,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-aosip
 
+# Power
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \

ItsVixano/android_device_xiaomi_whyred

Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

device.mk
@@ -361,7 +361,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3-service.xiaomi_sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Public Libraries
 PRODUCT_COPY_FILES += \