263db09c1bb7d82f3d837a5a4d2ded86994564d7
ItsVixano <vixanonew@gmail.com>
2021-02-20 21:19:47 +0100

    whyred: Kang light hal from Pe old tree
    * https://github.com/PixelExperience-Devices/device_xiaomi_whyred/tree/ten-bak
    * Acutally the current hal gives some issues like fast blinking of the battery led
    
    Change-Id: I9976d29ff6d025ebf150330461183b4b1e6b56bf

--- device.mk
+++ device.mk
@@ -305,7 +305,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sdm660
+    android.hardware.light@2.0-service.xiaomi_whyred
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
