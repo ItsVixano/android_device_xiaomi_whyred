8419e1473e42cecdca3f5089653fe62e07d03734
ItsVixano <vixanonew@gmail.com>
2021-03-19 22:40:14 +0100

    Revert "sdm660-common: Remove keymap of headset buttons"
    * Maybe some kernels don't have this patch applied "https://github.com/LineageOS/android_kernel_xiaomi_sdm660/commit/8183f8469ada98ff3a7c3f741bbe47dfcc9a8792"
    * Let's keep this for the time beeing
    
    This reverts commit 874001e92d88535f33f4ebe7aaa802a994eb95aa.
    
    Change-Id: I1fce50441f6617765434f7b704cac54c42d8b94b

--- device.mk
+++ device.mk
@@ -296,6 +296,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/keylayout/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc \
+    $(LOCAL_PATH)/keylayout/sdm660-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sdm660-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
