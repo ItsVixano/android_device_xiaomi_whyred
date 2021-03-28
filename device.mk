49049536261fb9d282c78404691a991596a6d91d
ItsVixano <vixanonew@gmail.com>
2021-03-28 18:49:53 +0200

    whyred: Drop seccomp
    * Already included on device/qcom/common
    
    Change-Id: Ic854562993b38d32cf0a87721552eee7f3891d29

--- device.mk
+++ device.mk
@@ -403,11 +403,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
